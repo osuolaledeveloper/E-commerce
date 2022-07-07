ce904f24222dcdd794c1445f2f0bbeee8776ad27
osuolaledeveloper <101516900+osuolaledeveloper@users.noreply.github.com>
2022-07-07 21:50:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("hello form index.js")
+    res.send("hello fffform index.js")
 })
 app.get("/admin/signup", (req, res) =>{
 res.send("<h1> Login</h1>")
